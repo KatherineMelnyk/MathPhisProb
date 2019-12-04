6755eae693114356c3c7115a458675cc4604dbbe
zeka <egavrilko@gmail.com>
2019-12-04 03:02:06 +0200

    Sphere done & fixed

--- Task.go
+++ Task.go
@@ -16,8 +16,8 @@ func main() {
 		cc     float64 = 445.
 		ro     float64 = 7800.
 		gamma  float64 = 140.
-		R      float64 = 0.5
-		T      float64 = 60 * 1500
+		R      float64 = 0.1
+		T      float64 = 60 * 15
 	)
 	v1 := func(t float64) float64 { return (u1(t) - u0) / u0 }
 
@@ -29,7 +29,7 @@ func main() {
 
 	// Numeric params
 	var (
-		N, M int     = 10, 4000
+		N, M int     = 10, 40
 		sig  float64 = 0.5
 	)
 
@@ -41,16 +41,17 @@ func main() {
 	}
 
 	var dx = make([]float64, N+1)
-	dx[0] = (x[1]*x[1] - x[0]*x[0]) / (2 * h)
-	dx[N] = (x[N]*x[N] - x[N-1]*x[N-1]) / (2 * h)
+	dx[0] = (x[1]*x[1]*x[1] - x[0]*x[0]*x[0]) / (3 * h)
+	dx[N] = (x[N]*x[N]*x[N] - x[N-1]*x[N-1]*x[N-1]) / (3 * h)
 
 	for i := 1; i < N; i++ {
-		dx[i] = (x[i+1]*x[i+1] - x[i-1]*x[i-1]) / (4 * h)
+		dx[i] = (x[i+1]*x[i+1]*x[i+1] - x[i-1]*x[i-1]*x[i-1]) / (6 * h)
 	}
 
 	var dp = make([]float64, N+1)
-	for i := 1; i < N; i++ {
-		dp[i] = x[i] - h/2
+	for i := 1; i <= N; i++ {
+		//dp[i] = (x[i] - h/2) * (x[i] - h/2)
+		dp[i] = (x[i] - h/2)
 	}
 
 	var y = make([]float64, N+1)
@@ -74,18 +75,18 @@ func main() {
 		xx := mat.NewDense(N+1, 1, nil)
 
 		b[0] = sig * tao / (h * h) * dp[1]
-		c[0] = -dx[0]*0.5 - b[0]
-		phi[0] = -0.5*dx[0]*y[0] - (1-sig)*tao/(h*h)*dp[1]*(y[1]-y[0])
+		c[0] = -dx[1]*0.5 - b[0]
+		phi[0] = -0.5*dx[1]*y[0] - (1-sig)*tao/(h*h)*dp[1]*(y[1]-y[0])
 
 		A.Set(0, 1, b[0])
 		A.Set(0, 0, c[0])
 		yy.Set(0, 0, phi[0])
 
 		for i := 1; i < N; i++ {
-			d[i] = -sig * tao / (h * h) * (x[i] - h/2)
-			b[i] = -sig * tao / (h * h) * (x[i+1] - h/2)
-			c[i] = x[i] - b[i] - d[i]
-			phi[i] = x[i]*y[i] + tao*(1-sig)/(h*h)*(dp[i+1]*(y[i+1]-y[i])-dp[i]*(y[i]-y[i-1]))
+			d[i] = -sig * tao / (h * h) * dp[i]
+			b[i] = -sig * tao / (h * h) * dp[i+1]
+			c[i] = dx[i] - b[i] - d[i]
+			phi[i] = dx[i]*y[i] + tao*(1-sig)/(h*h)*(dp[i+1]*(y[i+1]-y[i])-dp[i]*(y[i]-y[i-1]))
 
 			A.Set(i, i-1, d[i])
 			A.Set(i, i, c[i])
@@ -93,8 +94,8 @@ func main() {
 			yy.Set(i, 0, phi[i])
 		}
 		d[N] = sig * tao / (h * h) * dp[N]
-		c[N] = -sig*tao/h*gamma1*x[N] - 0.5*dx[N] - d[N]
-		phi[N] = (1-sig)*tao/h*gamma1*x[N]*y[N] - tao/h*x[N]*gamma1*v1(t) - 0.5*dx[N]*y[N] - (1-sig)*tao/(h*h)*dp[N]*(y[N]-y[N-1])
+		c[N] = -sig*tao/h*gamma1*x[N]*x[N] - 0.5*dx[N] - d[N]
+		phi[N] = (1-sig)*tao/h*gamma1*x[N]*x[N]*y[N] - tao/h*x[N]*x[N]*gamma1*v1(t) - 0.5*dx[N]*y[N] - (1-sig)*tao/(h*h)*dp[N]*(y[N]-y[N-1])
 
 		A.Set(N, N, c[N])
 		A.Set(N, N-1, d[N])
